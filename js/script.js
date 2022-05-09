05ced418b717be765a31cef1f1cb9f3a0e82585f
erfan-taraghi <taraghikonerfan@gmail.com>
2022-05-09 13:33:45 +0430

    part03 Error 404

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container');
+
+window.onmousemove = function(e){
+    let x = e.clientX / 5;
+    let y = e.clientY / 5;
+
+    
+
+    container.style.backgroundPositionX = x + 'px';
+    container.style.backgroundPositionY = y + 'px';
+}
